956256aa3c31c7ce498cee61ed66de7c11785877
HuangTengWei <teng.wei.huang0217@gmail.com>
2022-09-06 18:25:48 +0800

    [Add] Dynamic PM2.5 Threshold

--- app/src/main/java/com/andy/assignment/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/andy/assignment/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.andy.assignment.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.andy.assignment.orm.EPAHelper
@@ -18,7 +19,7 @@ class MainViewModel: ViewModel() {
 
     companion object {
         private val TAG = MainViewModel::class.java.simpleName
-        private val PM25_THRESHOLD = 16
+        private var PM25_THRESHOLD = 16
     }
 
     /**
@@ -38,25 +39,43 @@ class MainViewModel: ViewModel() {
                     }
                 }
 
+                updatePM25Threshold() // Demo Only
+
                 /*Take sites with pm2.5 larger than PM25_THRESHOLD*/
                 passAirSites.value = EPAHelper.mAirSites.filter{
                     it.pm2dot5.toIntOrNull()?.run {
                         this > PM25_THRESHOLD
                     } == true
                 }
+
                 /*Take sites with pm2.5 smaller than PM25_THRESHOLD and Empty pm2.5*/
                 unPassAirSites.value = EPAHelper.mAirSites.filter{
                         it.pm2dot5.toIntOrNull()?.run {
                         this <= PM25_THRESHOLD
                     } == true || it.pm2dot5.isEmpty()
                 }
+
                 hint.value = Pair(FETCH_STATUS.SUCCESS, "")
             } catch (err: Exception) {
+                err.printStackTrace()
                 passAirSites.value = mutableListOf()
                 unPassAirSites.value = mutableListOf()
                 hint.value = Pair(FETCH_STATUS.FAIL, err.message.toString())
             }
 
+        }
+    }
+
+    /**
+     * Only use for demo
+     * dynamic pick the threshold with median for close 50% 50%
+     */
+    private fun updatePM25Threshold() {
+        if (EPAHelper.mAirSites.size < 2) return
+        EPAHelper.mAirSites.sortedBy { it.pm2dot5.toIntOrNull()?:100 }.apply {
+            PM25_THRESHOLD = if (size % 2 == 1)
+                 get((size - 1 )/2).pm2dot5.toIntOrNull()?:16 else
+                (get((size / 2)-1).pm2dot5.toIntOrNull()?:30*0.5).toInt()
         }
     }
 
